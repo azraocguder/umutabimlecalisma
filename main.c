ca56677327bc06939e94b8ead1b52a64f5dfbfc6
azraocguder <152100279+azraocguder@users.noreply.github.com>
2023-11-26 23:47:58 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,46 +1,56 @@
-//
-//  main.c
-//  asd
-//
-//  Created by Azra Öçgüder on 26.11.2023.
-//
-
+// Online C compiler to run C program online
 #include <stdio.h>
-#include <stdbool.h>
 
-int main(int argc, const char * argv[]) {
-    // bir arrayin elemanlarının 33ğe bölümünden kalan2 olan sayıların indexlerinin çarpımının 4 e bölümünden kalanının karekökü arrayde mevut mu diye kontrol etsin
+int main() {
     int myNumbers[] = {25, 50, 75, 100, 200, 75};
-    int indekscarpimi = 0;
-    int indekscarpimibolumundenkalan = 0;
-    int indekscarpimibolumundenkalaninkarekoku = 0;
-    bool result = false;
     
-    for(int i = 0 ; i< 6; i++) {
-        if ( myNumbers[i] % 33 != 2) {
-           indekscarpimi = indekscarpimi * i;
+    float total = 0;
+    int count = 0;
+    
+    float totalcift = 0;
+    int countcift = 0;
+    
+    float result = 0;
+    float resultcift = 0;
+    //i0
+    //i1
+    //i2
+    //i3
+    //i4
+    //i5
+    for(int i = 0; i < 6; i = i+1) {
+       if (myNumbers[i] % 2 == 1) {
+            total = myNumbers[i] + total;
+            //total=25
+            //total=100
+            //total=175
+            count = count + 1 ;
+            //count=1
+            //count=2
+            //count=3
+        } else {
+            totalcift = myNumbers[i] + totalcift;
+            //totalcift=50
+            //totalcift=150
+            //totalcift=350
+            countcift = countcift + 1 ;
+            //countcift=1
+            //countcift=2
+            //countcift=3
         }
     }
-    indekscarpimibolumundenkalan = indekscarpimi % 4;
     
-    indekscarpimibolumundenkalaninkarekoku =  indekscarpimibolumundenkalan ^ 1/2;
+    result = total/ count;
+    // result=58.33
     
+    printf("%f", result);
     
+    printf("*******");
     
-    for(int i = 0 ; i< 6; i++) {
-        if ( myNumbers[i] == indekscarpimibolumundenkalaninkarekoku ) {
-            result = true;
-        }
-    }
+    resultcift = totalcift/ countcift;
+    //resultcift=116,6
     
-    
-    
-    if (result == true ) {
-        printf("mevcut");
-    } else {
-        printf("mevcut değil");
-    }
-    
+    printf("%f", resultcift);
     
     return 0;
 }
